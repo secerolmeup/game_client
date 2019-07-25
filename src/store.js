ef562163c1ae0075770f3234523a53578fb9eaee
IrsantyoHadi <irsantyohadi@gmail.com>
2019-07-25 22:02:11 +0700

    test

--- src/store.js
+++ src/store.js
@@ -1,16 +1,64 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import db from "./firebase/config"
+import { stat } from 'fs';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    Rooms : []
   },
   mutations: {
-
+    createRoom(state,payload){
+      console.log('sudah');
+    },
+    showRoom(state,payload){
+      state.Rooms = payload
+    }
   },
   actions: {
+    createNewRoom : function({ commit , state }, data){
+      let x = localStorage.getItem('username')
+      db.collection('lobby').add({
+        name : data.room,
+        players: [{
+          name : x,
+          position : 0
+        }],
+      })
+      .then(function(docRef){
+        localStorage.setItem('roomId', docRef.id)
+        commit('createRoom')
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    },
+    getExistRoom : function({commit, state}){
+      db.collection('lobby').onSnapshot(querySnapshot => {
+        let rooms = []
+        querySnapshot.forEach(doc => {
+          let newData = {
+            id : doc.id,
+            name: doc.data().name,
+            players: doc.data().players
+          }
+          rooms.unshift(newData)
+        });
+        commit('showRoom', rooms)
+      });
+    },
+    joinRoom({commit,state},payload){
+      let ayam = state.Rooms.filter(el => el.id == payload.id)
+      ayam[0].players.push(localStorage.getItem('username'))
 
+      db.collection('lobby').doc(payload.id).set(ayam[0])
+      .then(()=>{
+        console.log('berhasil');
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    }
   }
 })
