3e7075dd5db0517a51f61d6f1f441d79e91239d1
yudawardana <yuda.wardana40@gmail.com>
2019-07-26 03:56:47 +0700

    game sedikit lagi jadi, aturan2nya baru dibikin

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+<<<<<<< HEAD
 import Landing from './views/Landing.vue'
 // import Chat from '@/views/Chat.vue'
 // import Register from '@/views/Register.vue'
@@ -8,6 +9,9 @@ import Game from '@/views/game'
 import Chat from '@/views/Chat'
 
 
+=======
+import Game from '@/views/game'
+>>>>>>> game sedikit lagi jadi, aturan2nya baru dibikin
 Vue.use(Router)
 export default new Router({
   mode: 'history',
