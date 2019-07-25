b5bc38bc0330ad69ef668281602f35d77852e2e0
yudawardana <yuda.wardana40@gmail.com>
2019-07-26 03:56:47 +0700

    game sedikit lagi jadi, aturan2nya baru dibikin

--- src/router.js
+++ src/router.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Game from '@/views/game'
 Vue.use(Router)
-
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -20,6 +19,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component: Game
     }
   ]
 })
