effab85e25d26366369b5659aadeae579fbeaee9
imam madani <iafgon@yahoo.com>
2019-07-26 10:04:55 +0700

    ay

---
+++ src/components/PreviewPlayer.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Get player position</h1>
+        <button @click="getPlayerPos">click me!</button>
+    </div>
+</template>
+
+<script>
+import db from '../firebase/config'
+export default {
+		data() {
+			return{
+				player: {
+					position: 0,
+				}
+			}
+		},
+    methods: {
+        getPlayerPos() {
+            db
+							.collection('players')
+							.get()
+							.then((querySnapshot) => {
+									querySnapshot.forEach(player => {
+											console.log(player.data())
+											this.player.position = player.data().position
+									})
+							})
+							.catch(console.log)
+        }
+		}
+}
+</script>
+
+<style>
+
+</style>
