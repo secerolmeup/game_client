c7b843880ecfa29eeb8070914558593a2e43d7b8
yudawardana <yuda.wardana40@gmail.com>
2019-07-26 11:02:09 +0700

    hehe nambah kok

--- src/router.js
+++ src/router.js
@@ -19,20 +19,7 @@ export default new Router({
     //   component: Chat
     // },
     {
-      path: '/chat/:id',
-      name: 'chat',
-      component: Chat
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: Home
-    },
-    {
-      path: '/landing',
+      path: '/',
       name: 'landing',
       component: Landing
     },
