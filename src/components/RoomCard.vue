880afe1f230288a5443f773f9300256eb287aa09
yudawardana <yuda.wardana40@gmail.com>
2019-07-26 13:57:16 +0700

    sementara ini

---
+++ src/components/RoomCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="box-card">
+    <div slot="header" class="clearfix">
+      <p><span>{{room.name}}</span></p>
+      <el-button
+        @click="joinRoom(room.id)"
+        style="float: right; padding: 3px 0"
+        type="text"
+      >Join Room</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RoomCard",
+  props: ["room"],
+  created() {
+    console.log(this.room);
+  }
+};
+</script>
+
+<style>
+.box-card{
+  width: 180px;
+  height: 150px;
+  border: 3px solid brown;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* align-content: flex-end; */
+  justify-content: space-around;
+  margin: 20px;
+
+}
+</style>
